1a3f0376464655be36802f6f8ffa94eae7a4a461
nischal <nischal.com>
2026-09-19 00:15:05 +0530

    Fix aip

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -24,3 +25,4 @@ API.interceptors.request.use(
 );
 
 export default API;
+
